acdd17f45f6c2cf3cf123a7db0d71004aefdcf1f
joon0447 <94978051+joon0447@users.noreply.github.com>
2023-07-22 19:10:12 +0900

    Create 11654.java

---
+++ bronze/11654.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        char answer = sc.nextLine().charAt(0);
+        System.out.println((int)answer);
+        }
+    }
