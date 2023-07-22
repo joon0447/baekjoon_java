8b4de9e40d56b4fe6a8372a7b71f71f8ae713639
joon0447 <94978051+joon0447@users.noreply.github.com>
2023-07-22 11:58:20 +0900

    Create 10430.java

---
+++ bronze/10430.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = br.readLine().split(" ");
+        int A = Integer.parseInt(arr[0]);
+        int B = Integer.parseInt(arr[1]);
+        int C = Integer.parseInt(arr[2]);
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C)*(B%C))%C);
+    }
+}
