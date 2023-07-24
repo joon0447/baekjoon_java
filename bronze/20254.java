55aa6ee956f11c9c076e157070a6a11a10896206
joon0447 <94978051+joon0447@users.noreply.github.com>
2023-07-24 14:20:06 +0900

    Create 20254.java

---
+++ bronze/20254.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int answer = 56 * sc.nextInt() + 24 * sc.nextInt() + 14 *sc.nextInt() + 6 * sc.nextInt();
+        System.out.println(answer);
+    }
+}
+
