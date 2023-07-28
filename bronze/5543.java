6793f134244a1f6b30b82b1675425f7a106f008d
joon0447 <94978051+joon0447@users.noreply.github.com>
2023-07-28 11:30:01 +0900

    Create 5543.java

---
+++ bronze/5543.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+        int D = Integer.parseInt(br.readLine());
+        int E = Integer.parseInt(br.readLine());
+
+        int burger = Math.min(Math.min(A,B),C);
+        int drink = Math.min(D,E);
+        System.out.println(burger+drink-50);
+    }
+}
