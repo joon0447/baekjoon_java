8e956fb09d1b4ea915d1e36382af832cd38d22f2
joon0447 <94978051+joon0447@users.noreply.github.com>
2023-07-28 11:32:01 +0900

    Create 5554.java

---
+++ bronze/5554.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+        int D = Integer.parseInt(br.readLine());
+
+        int sec = A+B+C+D;
+        System.out.println(sec/60);
+        System.out.println(sec%60);
+    }
+}
