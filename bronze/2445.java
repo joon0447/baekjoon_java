7b3fff20397cc53dfd12f6f73ceee8f6d970ab49
joon0447 <94978051+joon0447@users.noreply.github.com>
2023-08-02 16:50:14 +0900

    Create 2445.java

---
+++ bronze/2445.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n - 1; i++) {
+            for (int k = 0; k < i + 1; k++) {
+                System.out.print("*");
+            }
+            for (int k = 0; k < 2 * n - 2 * (i + 1); k++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=0; i<2*n; i++){
+            System.out.print("*");
+        }
+        System.out.println();
+        for(int i=0; i<n-1; i++){
+            for(int k=0; k<n-(i+1); k++){
+                System.out.print("*");
+            }
+            for(int k=0; k<2*(i+1); k++) {
+                System.out.print(" ");
+            }
+            for(int k=0; k<n-(i+1); k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
