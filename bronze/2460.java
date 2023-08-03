96f197c56a4b79a4ab088cb57579051d0810542f
joon0447 <94978051+joon0447@users.noreply.github.com>
2023-08-03 20:14:18 +0900

    Create 2460.java

---
+++ bronze/2460.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = br.readLine().split(" ");
+        int total = Integer.parseInt(arr[1]);
+        int max = 0;
+        for(int i=0; i<9; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int out = Integer.parseInt(st.nextToken());
+            int in = Integer.parseInt(st.nextToken());
+            total += (in-out);
+            max = Math.max(max, total);
+        }
+        System.out.println(max);
+    }
+}
