6923599b0ab8e7e39fcec6f555f67483d9d88a0e
joon0447 <94978051+joon0447@users.noreply.github.com>
2023-08-04 17:29:45 +0900

    Create 8760.java

---
+++ bronze/8760.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int Z = Integer.parseInt(br.readLine());
+        for(int i=0; i<Z; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(a*b/2);
+        }
+
+    }
+}
