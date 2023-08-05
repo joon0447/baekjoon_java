f948898ea17c0633c1b8b10b869cce11fa73ea59
joon0447 <94978051+joon0447@users.noreply.github.com>
2023-08-05 17:27:17 +0900

    Create 10156.java

---
+++ bronze/10156.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        if(k*n>m){
+            System.out.println(k*n-m);
+        }else{
+            System.out.println(0);
+        }
+    }
+}
