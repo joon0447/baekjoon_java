66d817d3c585a67a4897357fb3b17dd4a96359dd
joon0447 <94978051+joon0447@users.noreply.github.com>
2023-08-06 19:26:17 +0900

    Create 2522.java

---
+++ bronze/2522.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0; i<n; i++){
+            for(int k=0; k<n-i-1; k++){
+                System.out.print(" ");
+            }
+            for(int k=0; k<=i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=0; i<n-1; i++){
+            for(int k=0; k<=i; k++){
+                System.out.print(" ");
+            }
+            for(int k=0; k<n-i-1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
