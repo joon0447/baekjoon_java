6976ef3a055e885c727f065693448b9cabe9f186
joon0447 <94978051+joon0447@users.noreply.github.com>
2024-01-09 16:19:54 +0900

    Create 10828.java

---
+++ sliver/10828.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i=0; i<n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            String str = st.nextToken();
+            int x = 0;
+            if(str.equals("push")){
+                x = Integer.parseInt(st.nextToken());
+            }
+            switch (str){
+                case "push":
+                    stack.push(x);
+                    break;
+                case "pop":
+                    if(stack.isEmpty()) System.out.println(-1);
+                    else{
+                        System.out.println(stack.pop());
+                    }
+                    break;
+                case "size":
+                    System.out.println(stack.size());
+                    break;
+                case "empty":
+                    if(stack.isEmpty()) System.out.println(1);
+                    else System.out.println(0);
+                    break;
+                case "top":
+                    if(stack.isEmpty()) System.out.println(-1);
+                    else{
+                        System.out.println(stack.peek());
+                    }
+                    break;
+            }
+        }
+    }
+}
+
