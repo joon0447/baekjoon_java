56b7a907228455ee856c79f305b3d26b60243e53
joon0447 <94978051+joon0447@users.noreply.github.com>
2024-01-10 15:39:20 +0900

    Create 10845.java

---
+++ sliver/10845.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+        int num = 0;
+        for(int i=0; i<n; i++){
+            String[] arr = br.readLine().split(" ");
+            String str = arr[0];
+            switch (str){
+                case "push":
+                    num = Integer.parseInt(arr[1]);
+                    queue.add(num);
+                    break;
+                case "pop":
+                    if(queue.isEmpty()) System.out.println(-1);
+                    else System.out.println(queue.poll());
+                    break;
+                case "size":
+                    System.out.println(queue.size());
+                    break;
+                case "empty":
+                    if(queue.isEmpty()) System.out.println(1);
+                    else System.out.println(0);
+                    break;
+                case "front":
+                    if(queue.isEmpty()) System.out.println(-1);
+                    else System.out.println(queue.peek());
+                    break;
+                case "back":
+                    if(queue.isEmpty()) System.out.println(-1);
+                    else System.out.println(num);
+                    break;
+            }
+        }
+    }
+}
