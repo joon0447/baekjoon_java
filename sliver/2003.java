409fbfce0357093d02a31755e6ad77d80dc0f361
joon0447 <94978051+joon0447@users.noreply.github.com>
2024-01-19 13:24:33 +0900

    Create 2003.java

---
+++ sliver/2003.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        int answer = 0;
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=0; i<n; i++){
+            int sum = 0;
+            for(int j=i; j<n; j++){
+                sum += arr[j];
+                if(sum == m){
+                    answer++;
+                }else if(sum>m){
+                    break;
+                }
+            }
+        }
+        System.out.println(answer);
+
+    }
+}
