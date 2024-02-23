7639b49288211623fe123771cbb731fa1e8c4d49
joon0447 <joon0447@naver.com>
2024-02-23 13:07:31 +0900

    0223

---
+++ sliver/1940.java
@@ -0,0 +1,29 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int answer = 0;
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=0; i<arr.length-1; i++){
+            for(int j = i+1; j<arr.length; j++){
+                if(arr[i] + arr[j] == m){
+                    answer++;
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
