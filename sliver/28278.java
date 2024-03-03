38a39f7934efc3b974a87592aa61a3489383834b
joon0447 <joon0447@naver.com>
2024-03-03 13:42:37 +0900

    0303

---
+++ sliver/28278.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+            switch (num){
+                case 1:
+                    stack.push(Integer.parseInt(st.nextToken()));
+                    break;
+                case 2:
+                    sb.append(stack.isEmpty() ? -1 : stack.pop()).append('\n');
+                    break;
+                case 3:
+                    sb.append(stack.size()).append('\n');
+                    break;
+                case 4:
+                    sb.append(stack.isEmpty() ? 1 : 0).append('\n');
+                    break;
+                case 5:
+                    sb.append(stack.isEmpty() ? -1 : stack.peek()).append('\n');
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
+}
