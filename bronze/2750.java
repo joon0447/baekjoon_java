6fb71b3efec34c653aaab735f3ea3ca9d554d010
joon0447 <joon0447@naver.com>
2024-04-28 12:28:51 +0900

    0428

---
+++ bronze/2750.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            int num = Integer.parseInt(br.readLine());
+            arr[i] = num;
+        }
+
+        Arrays.sort(arr);
+        StringBuilder sb = new StringBuilder();
+        for(int a : arr){
+            sb.append(a).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+}
